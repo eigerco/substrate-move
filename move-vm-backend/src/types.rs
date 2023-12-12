1a298f19d832a1962b0bca78711f1db4bff414fc
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-12-12 13:51:28 +0100

    feat: add GasStrategy enum for API

--- move-vm-backend/src/types.rs
+++ move-vm-backend/src/types.rs
@@ -68,3 +68,23 @@ impl VmResult {
         !self.is_ok()
     }
 }
+
+/// Gas is a resource-fuel for executing Move scripts.
+#[derive(Debug, Clone, Copy)]
+pub enum GasStrategy {
+    /// A metered gas with a provided limit.
+    ///
+    /// If the provided gas is not enough to execute the script or publish the script, then the
+    /// MoveVM will return the out-of-gas error message.
+    ///
+    /// This should be the standard option for the MoveVM.
+    Metered(u64),
+    /// It allows to run Move operations with an infinite amount of gas.
+    ///
+    /// This option should be used to estimate the required gas for the given MoveVM operation.
+    DryRun,
+    /// It allows to run the Move operations with the gas handling disabled.
+    ///
+    /// This option should be used only for testing and debugging purposes.
+    Unmetered,
+}
